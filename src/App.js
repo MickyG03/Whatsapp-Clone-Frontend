4ea64421dea272170b41aefae8d257cbd11abbb4
Mitesh Gulecha <mgulecha@iu.edu>
2023-08-14 01:41:15 -0400

    added routes and redux

--- src/App.js
+++ src/App.js
@@ -4,17 +4,28 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
+import { useDispatch } from 'react-redux';
+import { logout } from './features/userSlice';
 
 function App() {
+  const dispatch = useDispatch();
   return (
-  <Router>
-    <Routes>
-      <Route exact path="/" element={<Home/>} />
-      <Route exact path="/login" element={<Login/>} />
-      <Route exact path="/register" element={<Register/>} />
-    </Routes>
-    <div className='dark'></div>
-  </Router>
+    <div className='dark'>
+      <button
+        onClick={()=>{
+          dispatch(logout());
+        }}
+      >logout</button>
+
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/login" element={<Login/>} />
+          <Route exact path="/register" element={<Register/>} />
+        </Routes>
+        <div className='dark'></div>
+      </Router>
+  </div>
   );
 }
 
---
+++ src/app/store.js
@@ -0,0 +1,12 @@
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import userSlice from "../features/userSlice"
+
+const rootReducer = combineReducers({
+    user:userSlice,
+});
+
+export const store = configureStore({
+    reducer:rootReducer,
+    devTools:true,
+});
+
