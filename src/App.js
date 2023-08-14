38a96a7c5c70f6fe44dc56d25591dab9510a40c4
Mitesh Gulecha <mgulecha@iu.edu>
2023-08-14 02:17:29 -0400

    added user slices and redux persist

--- src/App.js
+++ src/App.js
@@ -4,19 +4,15 @@ import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import Home from "./pages/home";
 import Login from "./pages/login";
 import Register from "./pages/register";
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { logout } from './features/userSlice';
 
 function App() {
   const dispatch = useDispatch();
+  const {user}= useSelector((state)=>state.user)
+  console.log(user)
   return (
     <div className='dark'>
-      <button
-        onClick={()=>{
-          dispatch(logout());
-        }}
-      >logout</button>
-
       <Router>
         <Routes>
           <Route exact path="/" element={<Home/>} />
