890726df6ea3eec73bc7f1b003f7a4c0dc307ca7
Mitesh Gulecha <mgulecha@iu.edu>
2023-08-14 00:46:17 -0400

    added routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+
+//Pages import
+import Home from "./pages/home";
+import Login from "./pages/login";
+import Register from "./pages/register";
 
 function App() {
   return (
-    <div>
-      <h1 className="text-red-500 font-bold">Welcome</h1>
-    </div>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Home/>} />
+      <Route exact path="/login" element={<Login/>} />
+      <Route exact path="/register" element={<Register/>} />
+    </Routes>
+    <div className='dark'></div>
+  </Router>
   );
 }
 
