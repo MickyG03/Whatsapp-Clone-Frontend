b5d635a4783a980f0b650935aeb9f90913936423
Mitesh Gulecha <76037226+MickyG03@users.noreply.github.com>
2023-12-25 13:35:02 -0500

    changes for authentication

--- src/components/auth/Registerform.jsx
+++ src/components/auth/Registerform.jsx
@@ -1,15 +1,25 @@
-import { useForm } from "react-hook-form";
+ import { useForm } from "react-hook-form";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { signUpSchema } from "../../utils/validation";
 import AuthInput from "./AuthInput";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import PulseLoader from "react-spinners/PulseLoader";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { registerUser } from "../../features/userSlice";
 
 export default function RegisterForm(){
-    const {status}=useSelector((state)=>state.user)
+    const dispatch=useDispatch();
+    const navigate = useNavigate();
+    const {status,error}=useSelector((state)=>state.user)
     const { register, handleSubmit, watch, formState: { errors } } = useForm({resolver: yupResolver(signUpSchema)});
-    const onSubmit = data => console.log(data);
+    const onSubmit = async (data) => {
+        const res = await dispatch(registerUser({
+            ...data,
+            picture:"",
+
+        }));
+        if (res.payload.user ) navigate("/");
+    }
     return(
         <div className="h-screen w-full flex items-center justify-center overflow-hidden">
             <div className="max-w-md space-y-8 p-10 dark:bg-dark_bg_2 rounded-xl">
@@ -47,6 +57,11 @@ export default function RegisterForm(){
                     register={register}
                     error={errors?.password?.message}
                     />
+                    {error ? (
+                        <div>
+                        <p className="text-red-400">{error}</p>
+                        </div>
+                    ) : null}
                     <button className="w-full flex justify-center bg-green_1 text-gray-100 p-4 rounded-full tracking-wide font-semibold
                     focus:outline-none hover:bg-green_2 shadow-lg cursor-pointer transition ease-in duration-300" type="submit">
                         {status == "loading" ? (<PulseLoader color="#fff" size={16} />) : ("Sign Up")}
